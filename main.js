79e4fec5e9cefd0e958333aca430ae543ab1532d
megi-j <82594147+megi-j@users.noreply.github.com>
2021-10-13 19:02:23 +0400

    add some styles

---
+++ main.js
@@ -0,0 +1,19 @@
+let navigation = document.querySelectorAll('.nav-link');
+for(let i = 0; i < navigation.length; i++){
+    navigation[i].addEventListener('click', function(e){
+        console.log(this.href);
+        let href = this.getAttribute('href').slice(1);
+        let scrollPoint = document.getElementById(href).offsetTop;
+        e.preventDefault();
+        scrollDown(scrollPoint);
+    })
+}
+function scrollDown(max){
+    let scrollTop = document.querySelector('html').scrollTop;
+    if(scrollTop < max){
+        setTimeout(() => {
+            document.querySelector('html').scrollTop += 5;
+            scrollDown(max);
+        }, 2);
+    }
+}
